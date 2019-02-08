0dff3f3aee802467ef0c7212776d339ca001ce5d
Austin Cooley <adcooley@users.noreply.github.com>
2019-02-08 15:11:10 -0800

    tweaks

--- src/api/index.js
+++ src/api/index.js
@@ -4,10 +4,11 @@ import data from './data.js';
 export const getPets = async (_args) => {
   const pets = data;
 
-  // Simulates an asyc api that occasionally fails. 
+  // Simulates an async api
   await new Promise(resolve => setTimeout(resolve, _.random(300, 1500)));
-  if (_.random(0, 10) === 10) throw new Error('☹️');
-  // ==============================
+  
+  // Simulates receiving an error from the API. Please uncomment to test handling errors client-side.
+  // if (_.random(0, 10) === 10) throw new Error('☹️');
 
   return pets;
 }
