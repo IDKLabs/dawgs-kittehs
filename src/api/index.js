03d5645f6a4bf6041a8eb06b5f89cf31db85e71e
Emmy Armintrout <emmy@resource.io>
2019-02-08 10:59:46 -0800

    Update comment

--- src/api/index.js
+++ src/api/index.js
@@ -5,7 +5,6 @@ export const getPets = async (_args) => {
   const pets = data;
 
   // Simulates an asyc api that occasionally fails. 
-  // Feel free to comment this out if you start working on task #2. 
   await new Promise(resolve => setTimeout(resolve, _.random(300, 1500)));
   if (_.random(0, 10) === 10) throw new Error('☹️');
   // ==============================
