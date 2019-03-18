15211700b2b6c9d977fa1456bb3edbcc32a8c6be
Michelle <michmac202@gmail.com>
2019-03-18 13:32:46 -0700

    Infinite scroll with PetListItems

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,53 @@
 import _ from "lodash";
 import React, { Component } from "react";
 import "./app.scss";
-import { getPets } from "./api";
+import { getPets, getPetBatch } from "./api";
 import PetListItem from "./PetListItem.jsx";
 
 import InfiniteScroll from "react-infinite-scroller";
 import { List, message, Avatar, Spin } from "antd";
+import { exists } from "fs";
 
 class App extends Component {
   state = {
     pets: [],
-    loading: false,
-    hasMore: true
+    loading: true,
+    hasMore: true,
+    totalLoads: 0
   };
 
   constructor(props) {
     super(props);
     this.state = {
-      pets: []
+      pets: [],
+      loading: true,
+      hasMore: true,
+      totalLoads: 0
     };
   }
 
   componentDidMount = async () => {
-    const pets = await getPets(); // currently returns all pets
-    this.setState({ pets });
+    const pets = await getPetBatch(10, 0);
+    this.setState({ pets, loading: false });
   };
 
-  handleInfiniteOnLoad = () => {
+  handleInfiniteOnLoad = async page => {
+    console.log("Page number:", page);
+    //Limit loads (for now)
+    // const totalLoads = this.state.totalLoads + 1;
+    // this.setState({ totalLoads });
+
+    // if (this.state.totalLoads > 10) {
+    //   this.setState({ hasMore: false, loading: false });
+    //   return;
+    // }
+    // Prepare to load
     let pets = this.state.pets;
     this.setState({
       loading: true
     });
-    if (pets.length > 14) {
+    // Check if done loading everything
+    if (pets.length >= 1794) {
       message.warning("Infinite List loaded all");
       this.setState({
         hasMore: false,
@@ -39,21 +55,21 @@ class App extends Component {
       });
       return;
     }
-
-    // TODO: Get more pets, set loading to false
+    // Get a new batch
+    const newBatch = await getPetBatch(10, page);
+    pets = [...pets, ...newBatch];
+    this.setState({ pets, loading: false });
   };
 
   render() {
     const { pets } = this.state;
-
     return (
       <div className="infinite-container">
         <InfiniteScroll
-          initialLoad={false}
+          initialLoad={true}
           pageStart={0}
-          loadMore={this.handleInfiniteOnLoad}
+          loadMore={page => this.handleInfiniteOnLoad(page)}
           hasMore={!this.state.loading && this.state.hasMore}
-          useWindow={false}
         >
           {/* Header */}
           <div className="tableheader">
@@ -61,12 +77,10 @@ class App extends Component {
             <div className="name">Name</div>
             <div className="breed">Breed</div>
           </div>
-
           {/* List of pets */}
           {pets.map(({ id, ...pet }) => (
             <PetListItem key={id} pet={pet} />
           ))}
-
           {/* Loading message */}
           {this.state.loading && this.state.hasMore && (
             <div className="demo-loading-container">
@@ -76,15 +90,6 @@ class App extends Component {
         </InfiniteScroll>
       </div>
     );
-
-    // <div className="App">
-    //   <div className="tableheader">
-    //     <div className="type">Type</div>
-    //     <div className="name">Name</div>
-    //     <div className="breed">Breed</div>
-    //   </div>
-    //   { pets.map(({ id, ...pet }) => <PetListItem key={id} pet={pet} />) }
-    // </div>
   }
 }
 
--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,24 @@
-import _ from 'lodash';
-import data from './data.js';
+import _ from "lodash";
+import data from "./data.js";
 
-export const getPets = async (_args) => {
+export const getPets = async _args => {
   const pets = data;
 
   // Simulates an async api
   await new Promise(resolve => setTimeout(resolve, _.random(300, 1500)));
-  
+
   // Simulates receiving an error from the API. Please uncomment to test handling errors client-side.
   // if (_.random(0, 10) === 10) throw new Error('☹️');
 
   return pets;
-}
+};
+
+export const getPetBatch = async (batchSize, pagesLoaded) => {
+  pagesLoaded = pagesLoaded || 0; // NaN -> 0
+  const pets = data;
+  await new Promise(resolve => setTimeout(resolve, _.random(300, 1500)));
+
+  const start = pagesLoaded * 10;
+  console.log("returning pets", start, "through", start + batchSize);
+  return pets.slice(start, start + batchSize);
+};
